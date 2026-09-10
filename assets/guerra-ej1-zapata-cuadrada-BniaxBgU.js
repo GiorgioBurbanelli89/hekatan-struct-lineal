d12204158c060cdb9ad3c6d744a11a5c8041db9f
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-10 02:48:38 -0500

    CAD: el menu del clic derecho se cierra, y la ventana izq-der dice por que no coge

---
+++ assets/guerra-ej1-zapata-cuadrada-BniaxBgU.js
@@ -0,0 +1,43 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj1-2JiQBVXX.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-Cu22MOu-.js";
+import "./theme-Dxpmbnyd.js";
+import { __tla as __tla_2 } from "./didacticCpp-CnEP9H1T.js";
+import { __tla as __tla_3 } from "./deform-CK_Uh0DH.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./f2kPlateQ4-BZ9dGpgS.js";
+import "./cargaColumnaConsistente-DPcPMAlx.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-Druzr40L.js";
+import "./Text-DxjkL_3A.js";
+import "./styles-Ce_UnsFA.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-0QyZKvTL.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
