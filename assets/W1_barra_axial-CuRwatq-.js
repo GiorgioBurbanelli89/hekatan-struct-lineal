e11e0abb34d53ed11ede263e2306b31efea7f4bc
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-10 15:18:25 -0500

    Logo Struct visible en la barra

---
+++ assets/W1_barra_axial-CuRwatq-.js
@@ -0,0 +1,45 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { b as r, __tla as __tla_0 } from "./barraAxial-q58gMOX2.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CIzWOmGo.js";
+import "./analyze-DgLgRmKg.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-CnEP9H1T.js";
+import { __tla as __tla_3 } from "./deform-CK_Uh0DH.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./paramsSeccion-Bn-RQEWN.js";
+import "./cadSections-DVtTZU6U.js";
+import "./theme-Dxpmbnyd.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-pkkJffyc.js";
+import "./Text-DxjkL_3A.js";
+import "./styles-C05ElwtU.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-0QyZKvTL.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
---
+++ assets/getReport-CHUSw45h.js
@@ -0,0 +1,11 @@
+import { v as o } from "./theme-Dxpmbnyd.js";
+import { D as n } from "./styles-C05ElwtU.js";
+function a({ template: t, data: r }) {
+  const e = document.createElement("div");
+  return o.derive(() => {
+    n(t(r), e);
+  }), e;
+}
+export {
+  a as g
+};
