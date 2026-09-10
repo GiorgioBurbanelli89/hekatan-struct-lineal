1a1ed13e6764b759f0f33404fe2339a6e7072234
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-10 17:32:38 -0500

    La rejilla ya no le roba el encuadre a la camara

---
+++ assets/guerra-ej2-zapata-rectangular-sismo-Dx18ndh7.js
@@ -0,0 +1,43 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj2-9dtGT8RH.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-DxPW4U6i.js";
+import "./theme-Dxpmbnyd.js";
+import { __tla as __tla_2 } from "./didacticCpp-CnEP9H1T.js";
+import { __tla as __tla_3 } from "./deform-CK_Uh0DH.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./f2kPlateQ4-BZ9dGpgS.js";
+import "./cargaColumnaConsistente-DPcPMAlx.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B0x6YucZ.js";
+import "./Text-DxjkL_3A.js";
+import "./styles-DjzQZscE.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-DYymsd53.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
