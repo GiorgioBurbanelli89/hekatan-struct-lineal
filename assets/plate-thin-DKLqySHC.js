ec8afcda038b8732e9ee043e297973bb367cf5ec
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-13 15:25:02 -0500

    Deploy: panel de seleccion lee apoyos/cargas y se abre bajo la cinta

---
+++ assets/plate-thin-DKLqySHC.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { p as r, __tla as __tla_0 } from "./plateThin-BtT1nz50.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CH2XmDJ1.js";
+import { __tla as __tla_2 } from "./didacticCpp-CnEP9H1T.js";
+import { __tla as __tla_3 } from "./deform-CK_Uh0DH.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-DYUyfOZ6.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-BJWFR1R0.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
