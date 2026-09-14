8be322133715cf52f88ab90b9b8a78db86655b1d
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-14 15:29:40 -0500

    Tabla modal compacta: letra menor, sin saltos, ancho hasta ΣRz

---
+++ assets/triangular-plate-B9HHbbdv.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { t as r, __tla as __tla_0 } from "./triangularPlate-MmDukX3M.js";
+import { r as t, __tla as __tla_1 } from "./runExampleStandalone-CQYmAiBt.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./mitc3-2FJr2z_r.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-DYUyfOZ6.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-Cr9GeId9.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  t(r);
+});
