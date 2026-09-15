6a38567c509023ba89b9b82e380c0e3994d82a4c
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-15 03:06:02 -0500

    Tocar una barra: Ver K local 12x12 + script MATLAB (solver de main)

---
+++ assets/cad-draw-CiuKRLYu.js
@@ -0,0 +1,12 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { c as r } from "./cadDraw-C6PJzRpg.js";
+import { r as m } from "./runExampleStandalone-CGyoydvo.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B6v71d6U.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+m(r);
---
+++ assets/new-blank-BAk-EY4U.js
@@ -0,0 +1,43 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { n as r, __tla as __tla_0 } from "./newBlank-BDVNKfBE.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CGyoydvo.js";
+import "./theme-C-zoknmI.js";
+import "./analyze-DgLgRmKg.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-B6v71d6U.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
