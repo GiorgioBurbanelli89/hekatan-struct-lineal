0f128d1a64b77208a6f9bb1a74828a957813ec6d
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-15 03:20:28 -0500

    Tocar barra abre su K local; tooltip L con geometria original (solver de main)

---
+++ assets/columna-cft-DoqV65Ef.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { c as m, __tla as __tla_0 } from "./columnaCft-Bv-87GcZ.js";
+import { r as o, __tla as __tla_1 } from "./runExampleStandalone-DTz0o0fs.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./fiberSectionCft-CvKVMF85.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-E5b9g_4t.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  o(m);
+});
