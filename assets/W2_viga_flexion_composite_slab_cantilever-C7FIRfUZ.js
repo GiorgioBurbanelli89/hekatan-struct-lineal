755b68e4ab7ae120c761cbbc37c541308a604dac
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-15 01:50:36 -0500

    Diagrama 2D: la X cierra la ventana (solver de main)

---
+++ assets/W2_viga_flexion_composite_slab_cantilever-C7FIRfUZ.js
@@ -0,0 +1,44 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { v as o, __tla as __tla_0 } from "./vigaFlexionCompositeSlabCantilever-Q7kOH8l0.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-Cuj70aVu.js";
+import "./analyze-DgLgRmKg.js";
+import "./pureFunctionsAny.generated-DeJSBP3k.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./loadCaseHelpers-D2m4cQgV.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-D7ASuyih.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(o);
+});
---
+++ assets/plane-H-oMgnNf.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { p as r, __tla as __tla_0 } from "./plane-4MKPw-m3.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-Cuj70aVu.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./planeQ4-DsCzHfbV.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-D7ASuyih.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
