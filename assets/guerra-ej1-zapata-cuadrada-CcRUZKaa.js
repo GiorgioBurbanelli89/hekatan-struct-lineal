d14682aacb28327d80729bd0e072dc8db98354d4
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-17 00:44:47 -0500

    K local: SIEMPRE boton, tocar otra barra cierra la ventana

---
+++ assets/guerra-ej1-zapata-cuadrada-CcRUZKaa.js
@@ -0,0 +1,43 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj1-BG-0I_DS.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CMUkyljo.js";
+import "./theme-C-zoknmI.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./f2kPlateQ4-BZ9dGpgS.js";
+import "./cargaColumnaConsistente-DPcPMAlx.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-DAUjZ8kW.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
