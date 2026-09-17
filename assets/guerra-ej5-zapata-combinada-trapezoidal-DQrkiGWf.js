14ad5280b72bb5511bf56c03e235cce2d3e08ce0
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-17 00:31:31 -0500

    K local: al tocar una barra sale un boton, ya no se abre la ventana sola

---
+++ assets/guerra-ej5-zapata-combinada-trapezoidal-DQrkiGWf.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { g as r, __tla as __tla_0 } from "./guerraEj5-DExtIVaM.js";
+import { r as o, __tla as __tla_1 } from "./runExampleStandalone-CtAESNHF.js";
+import "./theme-C-zoknmI.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./f2kPlateQ4-BZ9dGpgS.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-Db5FzGH0.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  o(r);
+});
---
+++ assets/layered-shell-wN6XB51f.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { l as r, __tla as __tla_0 } from "./layeredShell-B8nubfIx.js";
+import { r as m, __tla as __tla_1 } from "./runExampleStandalone-CtAESNHF.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./layeredQ4-DuXBBCkf.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-Db5FzGH0.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  m(r);
+});
