377800c7bf6505379b9e21d94b2ac77c71dc842f
GiorgioBurbanelli89 <jbjazzjuventus1989@gmail.com>
2026-09-17 13:36:32 -0500

    Seleccion y raton como AutoCAD (arrastrar selecciona, rueda gira, derecho encuadra), Esc cancela de verdad, grillas auxiliares y ayuda con video

---
+++ assets/cad-draw-Dc0VjU6t.js
@@ -0,0 +1,12 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { c as r } from "./cadDraw-C6PJzRpg.js";
+import { r as m } from "./runExampleStandalone-BOZg4jAX.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-BgRQmEQi.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+m(r);
---
+++ assets/safe-bench-zapata-conectada-BeOz7Fn7.js
@@ -0,0 +1,42 @@
+import "./modulepreload-polyfill-B5Qt9EMX.js";
+import { s as o, __tla as __tla_0 } from "./safeBenchConectada-D7DkxPXb.js";
+import { r, __tla as __tla_1 } from "./runExampleStandalone-BOZg4jAX.js";
+import { __tla as __tla_2 } from "./didacticCpp-DH-WISVI.js";
+import { __tla as __tla_3 } from "./deform-BhbNzb1x.js";
+import "./preload-helper-V2P8TQsQ.js";
+import "./f2kPlateQ4-BZ9dGpgS.js";
+import "./theme-C-zoknmI.js";
+import "./tweakpane-BXg6ZhiP.js";
+import "./getViewer-BgRQmEQi.js";
+import "./Text-Cehu0nom.js";
+import "./styles-CqEyA8nI.js";
+import "./renderModalTable-CaPpqUR8.js";
+import "./units-DmFPJGLv.js";
+Promise.all([
+  (() => {
+    try {
+      return __tla_0;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_1;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_2;
+    } catch {
+    }
+  })(),
+  (() => {
+    try {
+      return __tla_3;
+    } catch {
+    }
+  })()
+]).then(async () => {
+  r(o);
+});
